823e99f4fab30c25c6bd3dc3791bac611ab5445f
Kirizaki <krajekg@gmail.com>
2025-09-02 15:40:22 +0200

    [add] ITK lib
    by building from sources:
    cmake .. \
      -DCMAKE_BUILD_TYPE=Release \
      -DBUILD_TESTING=OFF \
      -DBUILD_EXAMPLES=OFF \
      -DITK_BUILD_DEFAULT_MODULES=ON \
      -DModule_ITKVTK=ON \
      -DCMAKE_INSTALL_PREFIX=/usr/local/itk-5.4

--- include/mainwindow.hpp
+++ include/mainwindow.hpp
@@ -2,9 +2,21 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QMessageBox>
+
+// VTK includes
+#include <QVTKOpenGLNativeWidget.h>
 #include <vtkSmartPointer.h>
-class vtkGenericOpenGLRenderWindow;
-class vtkRenderer;
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkImageViewer2.h>
+
+// ITK includes
+#include <itkImage.h>
+#include <itkImageSeriesReader.h>
+#include <itkGDCMImageIO.h>
+#include <itkGDCMSeriesFileNames.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -20,9 +32,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void openDicom();
+
 private:
     Ui::MainWindow *ui;
     vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindow_;
     vtkSmartPointer<vtkRenderer> renderer_;
+    vtkSmartPointer<vtkImageViewer2> imageViewer_;
 };
 #endif // MAINWINDOW_H
